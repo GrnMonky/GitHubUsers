b212083556504e7d5eb8710f2f3eed21c7ce7c60
Landon Mann <mann.landon@gmail.com>
2024-04-02 20:29:53 +0900

    Initial Commit

---
+++ GitHubUsers/GitHubUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubUsersApp.swift
+//  GitHubUsers
+//
+//  Created by Landon Mann on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
